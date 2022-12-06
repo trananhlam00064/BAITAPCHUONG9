7019e837b510f7da86d9a40e7f742e5a5bcfdbc0
trananhlam00064 <119118231+trananhlam00064@users.noreply.github.com>
2022-12-06 22:43:13 +0700

    Create b9.8.py

---
+++ b9.8.py
@@ -0,0 +1,10 @@
+print ( "Nhập vào số N lớn hơn 0: " )
+n  =  int ( đầu vào ())
+tong  =  0
+cho  tôi  trong  phạm vi ( 1 , n ):
+    nếu ( n  %  i  ==  0 ):
+        tong  +=  tôi
+nếu ( tong  ==  n ):
+    print ( n , " is number perfect" )
+khác :
+    print ( n , "không phải là số hoàn hảo" )
