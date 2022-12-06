22c8e3bcf979e665f484085d1bbbe649697f9213
trananhlam00064 <119118231+trananhlam00064@users.noreply.github.com>
2022-12-06 22:37:59 +0700

    Create b9.7.py

---
+++ b9.7.py
@@ -0,0 +1,9 @@
+# chương trình trả về phần nguyên được phép chia 2 số nguyên
+nhập  toán  dưới dạng  neo
+def  chia_lay_nguyen ( x , y ):
+    z  =  x / y
+    z1  =  mới . tầng ( z )
+    in ( z1 )
+x  =  float ( input ( "nhập số x: " ))
+y  =  float ( input ( "nhập số y: " ))
+chia_lay_nguyen ( x , y )
